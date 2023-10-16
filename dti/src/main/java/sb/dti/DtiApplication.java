eca91bca45f8bd5f73550c775fec8cff320988ad
조흔나 <gms950831@gmail.com>
2023-10-16 09:47:03 +0900

    Initial commit

---
+++ dti/src/main/java/sb/dti/DtiApplication.java
@@ -0,0 +1,13 @@
+package sb.dti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtiApplication.class, args);
+	}
+
+}
---
+++ dti/src/test/java/sb/dti/DtiApplicationTests.java
@@ -0,0 +1,13 @@
+package sb.dti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
